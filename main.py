0e26afa70d9403f3c0df62c7d2917eaa2d22d7ba
Vyacheslav Tikhomirov <v.brudniy@yandex.ru>
2023-04-06 16:34:48 +0300

    added docstrings

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Докстринг для задания."""
     if char_class == 'warrior':
         damage = 5 + randint(3, 5)
     if char_class == 'mage':
@@ -14,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Докстринг для задания."""
     if char_class == 'warrior':
         block = 10 + randint(5, 10)
     if char_class == 'mage':
@@ -24,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Докстринг для задания."""
     if char_class == 'warrior':
         special = f'«Выносливость {80 + 25}»'
     if char_class == 'mage':
@@ -34,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Докстринг для задания."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -41,12 +47,10 @@ def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'healer':
         print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
     print('Потренируйся управлять своими навыками.')
-    print(
-        'Введи одну из команд:'
-        'attack — чтобы атаковать противника, '
-        'defence — чтобы блокировать атаку противника или '
-        'special — чтобы использовать свою суперсилу.'
-    )
+    print('Введи одну из команд:'
+          'attack — чтобы атаковать противника, '
+          'defence — чтобы блокировать атаку противника или '
+          'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd: str = str()
     while cmd != 'skip':
@@ -61,36 +65,31 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Докстринг для задания."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
-        char_class = input(
-            'Введи название персонажа, за которого хочешь играть: '
-            'Воитель — warrior, Маг — mage, Лекарь — healer: '
-        )
+        char_class = input('Введи название персонажа, '
+                           'за которого хочешь играть: '
+                           'Воитель — warrior, Маг — mage, '
+                           'Лекарь — healer: ')
         if char_class == 'warrior':
-            print(
-                'Воитель — дерзкий воин ближнего боя. '
-                'Сильный, выносливый и отважный.'
-            )
+            print('Воитель — дерзкий воин ближнего боя. '
+                  'Сильный, выносливый и отважный.')
         if char_class == 'mage':
-            print(
-                'Маг — находчивый воин дальнего боя. '
-                'Обладает высоким интеллектом.'
-            )
+            print('Маг — находчивый воин дальнего боя. '
+                  'Обладает высоким интеллектом.')
         if char_class == 'healer':
-            print(
-                'Лекарь — могущественный заклинатель. '
-                'Черпает силы из природы, веры и духов.'
-            )
-        approve_choice = input(
-            'Нажми (Y), чтобы подтвердить выбор, '
-            'или любую другую кнопку, чтобы выбрать другого персонажа '
-        ).lower()
+            print('Лекарь — могущественный заклинатель. '
+                  'Черпает силы из природы, веры и духов.')
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
+                               'или любую другую кнопку, '
+                               'чтобы выбрать другого персонажа ').lower()
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -100,6 +99,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-a = str()
+from math import sqrt
 
-print(type(a))
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Проверяет число на положительность."""
+    if your_number <= 0:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    message = ('Добро пожаловать в самую лучшую программу для вычисления '
+               'квадратного корня из заданного числа')
+    print(message)
+    your_number = 25.5
+    if calc(your_number) is True:
+        print(f'Мы вычислили квадратный корень из введённого вами числа. '
+              f'Это будет: {calculate_square_root(your_number)}')
